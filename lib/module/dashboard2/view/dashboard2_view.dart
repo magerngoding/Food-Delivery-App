9ab4b9ce226fc2f98afd62465538d5dea901e6e7
Hafizh <Hafizh@gmail.com>
2023-06-19 18:44:46 +0700

    Add dashboard 2

--- lib/module/dashboard2/view/dashboard2_view.dart
+++ lib/module/dashboard2/view/dashboard2_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:hyper_ui/core.dart';
-import '../controller/dashboard2_controller.dart';
+import '../../../shared/widget/card/product_rating_card/product_rating_card.dart';
+import '../../../shared/widget/uncategorized/location_text.dart';
 
 class Dashboard2View extends StatefulWidget {
   const Dashboard2View({Key? key}) : super(key: key);
@@ -10,14 +12,63 @@ class Dashboard2View extends StatefulWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Dashboard2"),
-        actions: const [],
+        leading: Container(),
+        leadingWidth: 0,
+        title: Stack(
+          children: [
+            Row(
+              children: [
+                Spacer(),
+                QLocationText(
+                  title: 'Delivery to',
+                  onPressed: () {
+                    Get.back();
+                  },
+                ),
+                Spacer(),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text("Filter"),
+                ),
+              ],
+            ),
+          ],
+        ),
+        actions: [],
       ),
       body: SingleChildScrollView(
         child: Container(
-          padding: const EdgeInsets.all(10.0),
+          padding: const EdgeInsets.all(20.0),
           child: Column(
-            children: const [],
+            children: [
+              StaggeredGrid.count(
+                crossAxisCount: 2,
+                mainAxisSpacing: 4,
+                crossAxisSpacing: 10,
+                children: List.generate(
+                  controller.products.length,
+                  (index) {
+                    var item = controller.products[index];
+                    bool evenLine = index % 2 == 0;
+                    return StaggeredGridTile.count(
+                      crossAxisCellCount: 1,
+                      mainAxisCellCount: evenLine ? 1.4 : 1.8,
+                      child: ProductRatingCard(
+                        image: item['photo'],
+                        title: item['product_name'],
+                        categories: [
+                          'Chinese',
+                          'Traditional',
+                        ],
+                        rating: 4.5,
+                        time: '15Min',
+                        delivery: 'Free',
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
           ),
         ),
       ),
--- lib/shared/widget/card/product_rating_card/product_rating_card.dart
+++ lib/shared/widget/card/product_rating_card/product_rating_card.dart
@@ -37,111 +37,111 @@ class ProductRatingCard extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Container(
-            height: 160.0,
-            width: 200,
-            clipBehavior: Clip.antiAlias,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage(
-                  image,
+          Expanded(
+            child: Container(
+              clipBehavior: Clip.antiAlias,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(
+                    image,
+                  ),
+                  fit: BoxFit.cover,
                 ),
-                fit: BoxFit.cover,
-              ),
-              borderRadius: BorderRadius.all(
-                Radius.circular(
-                  16.0,
-                ),
-              ),
-            ),
-            child: Stack(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.black.withOpacity(0.2),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(
+                    16.0,
                   ),
                 ),
-                Positioned(
-                  bottom: 10,
-                  left: 10,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          const Icon(
-                            MdiIcons.clock,
-                            size: 16.0,
-                            color: Colors.white,
-                          ),
-                          const SizedBox(
-                            width: 4.0,
-                          ),
-                          Text(
-                            time,
-                            style: TextStyle(
-                              fontSize: 12.0,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 6.0,
-                      ),
-                      Row(
-                        children: [
-                          const Icon(
-                            Icons.delivery_dining,
-                            size: 16.0,
-                            color: Colors.white,
-                          ),
-                          const SizedBox(
-                            width: 6.0,
-                          ),
-                          Text(
-                            delivery,
-                            style: TextStyle(
-                              fontSize: 12.0,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-                Positioned(
-                  right: 10,
-                  bottom: 10,
-                  child: Container(
-                    height: 20.0,
-                    width: 36,
+              ),
+              child: Stack(
+                children: [
+                  Container(
                     decoration: BoxDecoration(
-                      color: Colors.green,
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(
-                          6.0,
+                      color: Colors.black.withOpacity(0.2),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 10,
+                    left: 10,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            const Icon(
+                              MdiIcons.clock,
+                              size: 16.0,
+                              color: Colors.white,
+                            ),
+                            const SizedBox(
+                              width: 4.0,
+                            ),
+                            Text(
+                              time,
+                              style: TextStyle(
+                                fontSize: 12.0,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 6.0,
+                        ),
+                        Row(
+                          children: [
+                            const Icon(
+                              Icons.delivery_dining,
+                              size: 16.0,
+                              color: Colors.white,
+                            ),
+                            const SizedBox(
+                              width: 6.0,
+                            ),
+                            Text(
+                              delivery,
+                              style: TextStyle(
+                                fontSize: 12.0,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    right: 10,
+                    bottom: 10,
+                    child: Container(
+                      height: 20.0,
+                      width: 36,
+                      decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(
+                            6.0,
+                          ),
                         ),
                       ),
-                    ),
-                    child: Center(
-                      child: Text(
-                        '${rating}',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 12.0,
-                          fontWeight: FontWeight.w600,
+                      child: Center(
+                        child: Text(
+                          '${rating}',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 12.0,
+                            fontWeight: FontWeight.w600,
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
           const SizedBox(
-            height: 14.0,
+            height: 6.0,
           ),
           Text(
             title,
@@ -151,7 +151,7 @@ class ProductRatingCard extends StatelessWidget {
             ),
           ),
           const SizedBox(
-            height: 6.0,
+            height: 4.0,
           ),
           SingleChildScrollView(
             controller: ScrollController(),
