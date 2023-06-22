befa3d2b7f2c862dfc348f7856b194b82a09c876
Hafizh <Hafizh@gmail.com>
2023-06-22 15:26:55 +0700

    Add productlist 1

--- lib/module/product_list1/view/product_list1_view.dart
+++ lib/module/product_list1/view/product_list1_view.dart
@@ -1,6 +1,7 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+
 import 'package:hyper_ui/core.dart';
-import '../controller/product_list1_controller.dart';
 
 class ProductList1View extends StatefulWidget {
   const ProductList1View({Key? key}) : super(key: key);
@@ -10,15 +11,88 @@ class ProductList1View extends StatefulWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text("ProductList1"),
+        leading: Container(),
+        leadingWidth: 0,
+        title: const Text(
+          "Search",
+          style: TextStyle(fontSize: 20),
+        ),
         actions: const [],
       ),
       body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: const [],
-          ),
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.symmetric(
+                vertical: 6.0,
+                horizontal: 12.0,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(12.0),
+                ),
+                border: Border.all(
+                  width: 1.0,
+                  color: Colors.grey[400]!,
+                ),
+              ),
+              child: Row(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Icon(Icons.search),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      initialValue: null,
+                      decoration: const InputDecoration.collapsed(
+                        filled: true,
+                        fillColor: Colors.transparent,
+                        hoverColor: Colors.transparent,
+                        hintText: "Search on foodly",
+                      ),
+                      onFieldSubmitted: (value) {},
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
+            H3(
+              title: 'Top Restaurant',
+              onPressed: () {},
+            ),
+            const SizedBox(
+              height: 16.0,
+            ),
+            GridView.builder(
+              padding: EdgeInsets.zero,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                childAspectRatio: 1.0,
+                crossAxisCount: 2,
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 10,
+              ),
+              itemCount: controller.products.length,
+              shrinkWrap: true,
+              physics: ScrollPhysics(),
+              itemBuilder: (BuildContext context, int index) {
+                var item = controller.products[index];
+                return CardSearching(
+                  image: item['photo'],
+                  title: item['product_name'],
+                  category: item['category'],
+                );
+              },
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+          ],
         ),
       ),
     );
@@ -26,4 +100,77 @@ class ProductList1View extends StatefulWidget {
 
   @override
   State<ProductList1View> createState() => ProductList1Controller();
+}
+
+class CardSearching extends StatelessWidget {
+  final String image;
+  final String title;
+  final String category;
+
+  const CardSearching({
+    Key? key,
+    required this.image,
+    required this.title,
+    required this.category,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 180,
+      width: 160,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(
+                    image,
+                  ),
+                  fit: BoxFit.cover,
+                ),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(
+                    8,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 16.0,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          const SizedBox(
+            height: 4.0,
+          ),
+          Row(
+            children: [
+              Text(
+                "\$\$",
+                style: TextStyle(
+                  fontSize: 14.0,
+                ),
+              ),
+              DotCenter(),
+              Text(
+                category,
+                style: TextStyle(
+                  fontSize: 14.0,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 }
