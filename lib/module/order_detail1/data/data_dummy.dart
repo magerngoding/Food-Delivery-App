83dff2fd75f73bd584b827090f24d499d6d7180f
Hafizh <Hafizh@gmail.com>
2023-08-09 09:40:50 +0700

    Add orderDetail1

---
+++ lib/module/order_detail1/data/data_dummy.dart
@@ -0,0 +1,22 @@
+class OrderDetailData {
+  static List orderDetailList = [
+    {
+      'quantity': 1,
+      'product_name': 'Cookie Sandwich',
+      'description': 'Shortbread, choocolate',
+      'product_price': 7.4,
+    },
+    {
+      'quantity': 1,
+      'product_name': 'Combo Buger',
+      'description': 'Shortbread, choocolate',
+      'product_price': 12,
+    },
+    {
+      'quantity': 3,
+      'product_name': 'Oyster Dish',
+      'description': 'Shortbread, choocolate',
+      'product_price': 5.6,
+    },
+  ];
+}
--- lib/module/order_detail1/view/order_detail1_view.dart
+++ lib/module/order_detail1/view/order_detail1_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hyper_ui/core.dart';
-import '../controller/order_detail1_controller.dart';
+import 'package:hyper_ui/module/order_detail1/data/data_dummy.dart';
 
 class OrderDetail1View extends StatefulWidget {
   const OrderDetail1View({Key? key}) : super(key: key);
@@ -17,7 +17,227 @@ class OrderDetail1View extends StatefulWidget {
         child: Container(
           padding: const EdgeInsets.all(10.0),
           child: Column(
-            children: const [],
+            children: [
+              Row(
+                children: [
+                  const Icon(
+                    Icons.close_rounded,
+                    size: 24.0,
+                  ),
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Your Orders",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 41.0,
+              ),
+              ListView.builder(
+                shrinkWrap:
+                    true, // Kasih shrinkwrap jika parent ada SingleScrollScroll
+                itemCount: OrderDetailData.orderDetailList.length,
+                physics: const ScrollPhysics(),
+                itemBuilder: (BuildContext context, int index) {
+                  var item = OrderDetailData.orderDetailList[index];
+                  return Column(
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.all(8.0),
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                width: 0.5,
+                                color: Color.fromRGBO(134, 134, 134, 1),
+                              ),
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(4.0),
+                              ),
+                            ),
+                            child: Text(
+                              item['quantity'].toString(),
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.bold,
+                                color: primaryColor,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 16.0,
+                          ),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  item['product_name'],
+                                  style: TextStyle(
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 8.0,
+                                ),
+                                Text(
+                                  item['description'],
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(
+                                    fontSize: 16.0,
+                                    color: secondaryTextColor,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Text(
+                            'USD ${item['product_price'].toString()}',
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.bold,
+                              color: primaryColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Divider(
+                        height: 40,
+                        color: Color.fromRGBO(103, 103, 103, 0.1),
+                      ),
+                    ],
+                  );
+                },
+              ),
+              const SizedBox(
+                height: 17.0,
+              ),
+              Row(
+                children: [
+                  Text(
+                    "Subtotal",
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Spacer(),
+                  Text(
+                    "\$29.4",
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 15.0,
+              ),
+              Row(
+                children: [
+                  Text(
+                    "Delivery",
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Spacer(),
+                  Text(
+                    "\$20",
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 15.0,
+              ),
+              Row(
+                children: [
+                  Text(
+                    "Total(incl. VAT)",
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Spacer(),
+                  Text(
+                    "\$29.4",
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 24.0,
+              ),
+              Row(
+                children: [
+                  Text(
+                    "Add more items",
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                      color: primaryColor,
+                    ),
+                  ),
+                  Spacer(),
+                  const Icon(
+                    Icons.chevron_right,
+                    size: 24.0,
+                  ),
+                ],
+              ),
+              Divider(
+                height: 40,
+                color: Color.fromRGBO(103, 103, 103, 0.1),
+              ),
+              Row(
+                children: [
+                  Text(
+                    "Promo Code",
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                      color: primaryColor,
+                    ),
+                  ),
+                  Spacer(),
+                  const Icon(
+                    Icons.chevron_right,
+                    size: 24.0,
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 46.0,
+              ),
+              QBtutton(
+                label: 'Continue (\$11.98)',
+                onPressed: () {},
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+            ],
           ),
         ),
       ),
